c6f83850f742e6c9e633fce9d3679a599bc34a74
TLOMadness <85772798+TLOMadness@users.noreply.github.com>
2021-06-27 02:30:04 -0500

    got bryces things

--- Assets/Scripts/Dialogue/Bryce_Day2_0.cs
+++ Assets/Scripts/Dialogue/Bryce_Day2_0.cs
@@ -2,35 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Dax_Day1 : Dialogue
+public class Bryce_Day2_0 : Dialogue
 {
 
     protected override void DEvent0()
     {
-        TextboxManager.Instance.SetLine(10);
+        TextboxManager.Instance.SetLine(5);
     }
 
     protected override void DEvent1()
     {
+        TextboxManager.Instance.SetLine(10);
     }
 
     protected override void DEvent2()
-    {;
+    {
+        TextboxManager.Instance.SetLine(15);
     }
     protected override void DEvent3()
     {
+        TextboxManager.Instance.SetLine(21);
     }
     protected override void DEvent4()
     {
+        //invite bryce to bar
     }
     protected override void DEvent5()
     {
-
+        //invite bryce to karaoke
     }
     protected override void DEvent6()
-    
-{
+    {
+        //invite bryce to pavilion
     }
-
 
 }
