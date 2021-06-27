f086a0be96dded47574da998b7676d4105f86e10
TLOMadness <85772798+TLOMadness@users.noreply.github.com>
2021-06-27 00:57:35 -0500

    Dax script

--- Assets/Scripts/Dialogue/Bartender.cs
+++ Assets/Scripts/Dialogue/Bartender.cs
@@ -2,25 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Dax : Dialogue
+public class Bartender : Dialogue
 {
 
     protected override void DEvent0()
     {
+        TextboxManager.Instance.SetLine(7);
     }
 
     protected override void DEvent1()
     {
+        TextboxManager.Instance.SetLine(25);
     }
 
     protected override void DEvent2()
-    {;
+    {
+        TextboxManager.Instance.SetLine(15);
     }
     protected override void DEvent3()
     {
+        TextboxManager.Instance.SetLine(28);
     }
     protected override void DEvent4()
     {
+        TextboxManager.Instance.SetLine(33);
     }
     protected override void DEvent5()
     {
