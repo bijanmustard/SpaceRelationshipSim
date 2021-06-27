e35a7e20f7e9f83232c082eb61d0484817a784c8
TLOMadness <85772798+TLOMadness@users.noreply.github.com>
2021-06-27 04:16:46 -0500

    Added bryce part2

---
+++ Assets/Scripts/Dialogue/Bryce/Bryce_Day2_Karaoke.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bryce_Day2_Karaoke : Dialogue
+{
+
+
+
+    protected override void DEvent0()
+    {
+
+    }
+
+    protected override void DEvent1()
+    {
+        
+    }
+
+    protected override void DEvent2()
+    {
+        
+    }
+}
