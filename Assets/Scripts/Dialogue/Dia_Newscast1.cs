90fae6f4609bcfba23b284ec8903e7da86604b05
TLOMadness <85772798+TLOMadness@users.noreply.github.com>
2021-06-27 03:01:52 -0500

    Day one news

--- Assets/Scripts/Dialogue/Dia_Newscast1.cs
+++ Assets/Scripts/Dialogue/Dia_Newscast1.cs
@@ -4,5 +4,18 @@ using UnityEngine;
 
 public class Dia_Newscast1 : Dialogue
 {
-   
+
+
+
+    protected override void DEvent0()
+    {
+        if (!Game_Manager.Instance.flags.daxDeath) TextboxManager.Instance.SetLine(5);
+        else TextboxManager.Instance.SetLine(12);
+    }
+
+    protected override void DEvent1()
+    {
+        TextboxManager.Instance.SetLine(18);
+    }
+
 }
